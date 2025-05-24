8cc8490aefcecd6b43b75e107d3db60156401e56
danya.pac3 <danya.pac3@gmail.com>
2025-05-24 22:33:25 +0300

    Add swithing between pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import template from '/pages/contacts.html';
+import contactsHtml from '/pages/contacts.html';
+import homeHtml from '/pages/home.html';
+import productsHtml from '/pages/products.html';
 import { renderPage } from "/js/utils";
+
+const pages = {
+  $home: renderPage(homeHtml),
+  $products: renderPage(productsHtml),
+  $contacts: renderPage(contactsHtml),
+};
+
+const $homeButton = document.querySelector(".to-home");
+const $productsButton = document.querySelector(".to-products");
+const $contactsButton = document.querySelector(".to-contacts");
 const $content = document.querySelector("#content");
 
-$content.appendChild(renderPage(template));
+$homeButton.addEventListener('click', () => {
+  $content.replaceChildren(pages.$home);
+});
+
+$productsButton.addEventListener('click', () => {
+  $content.replaceChildren(pages.$products);
+});
+
+$contactsButton.addEventListener('click', () => {
+  $content.replaceChildren(pages.$contacts);
+});
