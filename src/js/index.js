1d2ba740d15b306b84b120a27064e907583eb7b4
danya.pac3 <danya.pac3@gmail.com>
2025-05-26 09:39:31 +0300

    Improve switching between pages

--- src/js/index.js
+++ src/js/index.js
@@ -5,25 +5,40 @@ import homeHtml from '/pages/home.html';
 import productsHtml from '/pages/products.html';
 import { renderPage } from "/js/utils";
 
+
 const pages = {
   $home: renderPage(homeHtml),
   $products: renderPage(productsHtml),
   $contacts: renderPage(contactsHtml),
 };
 
+
 const $homeButton = document.querySelector(".to-home");
 const $productsButton = document.querySelector(".to-products");
 const $contactsButton = document.querySelector(".to-contacts");
 const $main = document.querySelector(".main");
+const $headerButtons = document.querySelectorAll(".header__button");
 
-$homeButton.addEventListener('click', () => {
-  $main.replaceChildren(pages.$home);
-});
 
-$productsButton.addEventListener('click', () => {
-  $main.replaceChildren(pages.$products);
-});
+const mountPage = ($page) => $main.replaceChildren($page);
 
-$contactsButton.addEventListener('click', () => {
-  $main.replaceChildren(pages.$contacts);
-});
+const clickHandler = (e) => {
+  const $target = e.target;
+  if ($target.classList.contains('to-home'))
+    mountPage(pages.$home);
+  else if ($target.classList.contains('to-products'))
+    mountPage(pages.$products);
+  else if ($target.classList.contains('to-contacts'))
+    mountPage(pages.$contacts);
+
+  document.querySelector(".header__button.active").classList.remove('active');
+  $target.classList.add('active');
+}
+
+
+$homeButton.addEventListener('click', clickHandler);
+$productsButton.addEventListener('click', clickHandler);
+$contactsButton.addEventListener('click', clickHandler);
+
+
+mountPage(pages.$home);
